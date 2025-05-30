df020ef72cb26ec0f855c1b2f808bfe2a994cc6c
Priyansh-K <priyanshu.koirala77@gmail.com>
2025-05-30 07:31:00 +0545

    Refactor code structure for improved readability and maintainability

---
+++ detection/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from Utils import predictor, retrainer
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"response": "Welcome to the URL predictor API. To view documentation, visit /docs"}
+
+@app.get("/retrain")
+def retrain():
+    return retrainer.run()
+
+@app.get("/isrunning")
+def isrunning():
+    return retrainer.is_running()
+
+@app.get("/predict")
+def predict(url: str):
+    return {"result": "benign"} if predictor.predict(url) == 0 else {"result": "malicious"}
